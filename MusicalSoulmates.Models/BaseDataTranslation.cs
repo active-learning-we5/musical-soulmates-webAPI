e46fd28e52c41aaf10b9dbd6911109232086bb82
reza-baiat <reza.baiat88@gmail.com>
2023-06-01 11:13:15 -0700

    [base data] models defined

---
+++ MusicalSoulmates.Models/BaseDataTranslation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace MusicalSoulmates.Models
+{
+    public class BaseDataTranslation
+    {
+        public BaseData BaseData { get; set; }
+        public string Lang { get; set; }
+        public string Title { get; set; }
+    }
+}
