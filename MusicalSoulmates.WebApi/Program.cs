7da316764836efdf26a2204afcf79763028ccc1e
reza-baiat <reza.baiat88@gmail.com>
2023-06-01 11:14:29 -0700

    [db context] using db context in web api project

--- MusicalSoulmates.WebApi/Program.cs
+++ MusicalSoulmates.WebApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using MusicalSoulmates.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MSDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MSDb")));
 
 var app = builder.Build();
 
