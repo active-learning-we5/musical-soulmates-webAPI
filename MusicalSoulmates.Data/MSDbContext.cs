b5792d27f802709b89514921ca838ae228812843
reza-baiat <reza.baiat88@gmail.com>
2023-06-01 11:14:00 -0700

    [db context] dbContext defined

---
+++ MusicalSoulmates.Data/MSDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicalSoulmates.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicalSoulmates.Data
+{
+    public class MSDbContext : DbContext
+    {
+        public MSDbContext() : base()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+        public DbSet<BaseData> BaseData { get; set; }
+        public DbSet<BaseDataTranslation> BaseDataTranslations { get; set; }
+    }
+}
