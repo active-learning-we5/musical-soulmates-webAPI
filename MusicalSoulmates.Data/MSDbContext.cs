2a7cd7a6c8cb79985200a21e139ccbac77dd61c5
yasaminashoori <yasaminashoori18@gmail.com>
2023-06-30 14:09:52 +0430

    Add Migrations

--- MusicalSoulmates.Data/MSDbContext.cs
+++ MusicalSoulmates.Data/MSDbContext.cs
@@ -17,11 +17,21 @@ namespace MusicalSoulmates.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP-FDV7DG9;Database=SoulmatesDB;User Id=sa;Password=2580;");
+            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<BaseData>()
+        .HasKey(bd => bd.Id);
+
+            modelBuilder.Entity<BaseData>()
+                .HasMany(bd => bd.BaseDataTranslations)
+                .WithOne(bdt => bdt.BaseData)
+                .HasForeignKey(bdt => bdt.BaseDataId);
         }
 
         public DbSet<BaseData> BaseData { get; set; }
---
+++ MusicalSoulmates.Data/Migrations/20230630093638_DbContext.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MusicalSoulmates.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DbContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BaseData",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ParentId = table.Column<int>(type: "int", nullable: false),
+                    Order = table.Column<int>(type: "int", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BaseData", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BaseData_BaseData_ParentId",
+                        column: x => x.ParentId,
+                        principalTable: "BaseData",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BaseDataTranslations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BaseDataId = table.Column<int>(type: "int", nullable: false),
+                    Lang = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BaseDataTranslations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BaseDataTranslations_BaseData_BaseDataId",
+                        column: x => x.BaseDataId,
+                        principalTable: "BaseData",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BaseData_ParentId",
+                table: "BaseData",
+                column: "ParentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BaseDataTranslations_BaseDataId",
+                table: "BaseDataTranslations",
+                column: "BaseDataId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BaseDataTranslations");
+
+            migrationBuilder.DropTable(
+                name: "BaseData");
+        }
+    }
+}
